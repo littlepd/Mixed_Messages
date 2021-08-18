c4ab3cce4fc989c0c1bca1351fe13c6834f62c0b
Michael Daly <13458580+littlepd@users.noreply.github.com>
2021-08-18 16:42:40 -0500

    Added more quotes and randomized their output.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-console.log("'I spent 90 percent of my money on women and drink. The rest I wasted Betting on the Knicks.' - Geroge Best");
-console.log("'I feel like I'm the best, but you're not going to get me to say that.' - Jerry Rice");
+let quotes = ["'I spent 90 percent of my money on women and drink. The rest I wasted Betting on the Knicks.' - Former Footballer Geroge Best",
+              "'I feel like I'm the best, but you're not going to get me to say that.' - Hall of Fame NFL Receiver Jerry Rice",
+              "'My career was sputtering until I did a 360 and got headed in the right direction.' - NBA Guard Tracy McGrady",
+              "'He treats us like men. He lets us wear earrings.' - Former College Receiver Torrin Polk",
+              "'The drivers have one foot on the brake, one on the clutch and one on the throttle.' - Speed Channel Host Bob Varsha",
+              "'We're going to turn this team around 360 degrees.' - NBA Guard Jason Kidd",
+              "'The NFL, like life, is full of idiots.' - Former NFL Offensive Lineman Randy Cross",
+              "'I'm glad I don't play anymore. I could never learn all of those handshakes.' - Hall of Fame Shortstop Phil Rizzuto",
+              "'It took me 17 years to get 3,000 hits in baseball. I did it in one afternoon on the golf course.' - Hank Aaron",
+              "'He's a guy who gets up at six o'clock in the morning regardless of what time it is.' - Boxing Trainer Lou Duva",
+              "'Golf and sex are the only things you can enjoy without being good at them.' - Three-Time Masters Champion Jimmy Demaret",
+              "'I'm not allowed to comment on lousy officiating.' - Former Saints General Manager Jim Fink",
+              "'You guys line up alphabetically by height' - ormer College Football Coach Bill Peterson",
+              "'I was thinking about making a comeback, until I pulled a muscle vacuuming.' - Hall of Fame MLB Catcher Johnny Bench",
+              "'I went to a fight the other night, and a hockey game broke out.' - Comedian Rodney Dangerfield",
+              "'They say that nobody is perfect. Then they tell you practice makes perfect. I wish they'd make up their minds.' - Hall of Fame NBA Center Wilt Chamberlain",
+              "'We have a great bunch of outside shooters. Unfortunately, all our games are played indoors.' - Former College Basketball Coach Weldon Drew",
+              "'I want to rush for 1,000 or 1,500 yards. Whichever comes first.' - Former NFL Running Back George Rogers",
+              "'I was such a dangerous hitter I even got intentional walks in batting practice.' - MLB Icon Casey Stengel",
+              "'Most football players are temperamental. That's 90 percent temper and 10 percent mental.' - Former NFL Safety Doug Plank",
+              "'Any time Detroit scores more than 100 points and holds the other team below 100 points, they almost always win.' - NBA Coach Doug Collins",
+              "'I've seen George Foreman shadow boxing and the shadow won.' - Legendary Boxer Muhammad Ali",
+              "'I quit school in the sixth grade because of pneumonia. Not because I had it, but because I couldn't spell it.' - Heavyweight Legend Rocky Graziano",
+              "'My sister’s expecting a baby, and I don’t know if I’m going to be an uncle or an aunt.' - Former NBA Center Chuck Nevitt",
+              "'All hockey players are bilingual. They know English and profanity.' - NHL Legend Gordie Howe",
+              "'Ninety percent of this game is half mental.' - Hall of Fame MLB Catcher Yogi Berra",
+              "'I may be dumb, but I'm not stupid.' - Hall of Fame NFL Quarterback Terry Bradshaw",
+              "'It's just a job. Grass grows, birds fly, waves pound the sand. I beat people up.' - Boxing Legend Muhammad Ali"];
 
+console.log(quotes[Math.floor(Math.random() * quotes.length)]);
