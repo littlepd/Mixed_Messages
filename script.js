4a60597497ce28b93d2ff127e4b0834ee0d13bd9
Michael Daly <13458580+littlepd@users.noreply.github.com>
2021-08-18 16:12:35 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("'I spent 90 percent of my money on women and drink. The rest I wasted Betting on the Knicks.' - Geroge Best");
+console.log("'I feel like I'm the best, but you're not going to get me to say that.' - Jerry Rice");
+
